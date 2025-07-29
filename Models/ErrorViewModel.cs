fa3382aba5906d55f3d9984fe01bf2b79c59a3cd
SoumenNath <soumennath120@gmail.com>
2025-07-29 18:28:45 -0400

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DisasterResourceFinder.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/Resource.cs
@@ -0,0 +1,21 @@
+﻿namespace DisasterResourceFinder.Models
+{
+    public class Resource
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string PostalCode { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public string Phone { get; set; }
+        public string WebsiteUrl { get; set; }
+        public string OpeningHours { get; set; }
+        public int? Capacity { get; set; }
+        public bool IsWheelchairAccessible { get; set; }
+        public bool IsPetFriendly { get; set; }
+        public DateTime LastUpdated { get; set; }
+    }
+}
